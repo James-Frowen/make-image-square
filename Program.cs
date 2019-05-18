a1939a92cce43dc4550c7d588db6a6f5ec9360b1
JamesFrowen-PC\JamesFrowen <jamesfrowendev@gmail.com>
2019-05-18 13:42:56 +0100

    code

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace MakeImageSquare
+{
+    internal class Program
+    {
+        private const int size = 1024;
+
+        private static void Main(string[] args)
+        {
+            var path = args[0];
+            using (var img = (Bitmap)Image.FromFile(path))
+            {
+                using (var output = new Bitmap(size, size, PixelFormat.Format32bppArgb))
+                {
+                    var width = img.Width;
+                    var height = img.Height;
+
+                    var offsetX = (width - size) / 2;
+                    var offsetY = (height - size) / 2;
+
+                    for (int x = 0; x < size; x++)
+                    {
+                        for (int y = 0; y < size; y++)
+                        {
+                            var InX = x + offsetX;
+                            var InY = y + offsetY;
+                            Color c;
+                            if (InX < 0 || InX >= width || InY < 0 || InY >= height)
+                            {
+                                c = Color.Transparent;
+                            }
+                            else
+                            {
+                                c = img.GetPixel(InX, InY);
+                            }
+
+                            output.SetPixel(x, y, c);
+                        }
+                    }
+
+                    var folder = Path.GetDirectoryName(path);
+                    var withoutExtension = Path.GetFileNameWithoutExtension(path);
+                    var newPath = Path.Combine(folder, withoutExtension + "_Square.png");
+                    output.Save(newPath);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# make-image-square
+
+Converts a screen shot to a 1024 sqaure image
